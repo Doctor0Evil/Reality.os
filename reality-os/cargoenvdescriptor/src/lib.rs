6eb7de61ae9e03e2bf74d02f2c3bca609ad5826d
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-04 12:00:50 -0700

    Create lib.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ reality-os/cargoenvdescriptor/src/lib.rs
@@ -0,0 +1,12 @@
+impl CargoEnvDescriptor {
+    pub fn current_for_neurostack() -> Self {
+        // ...
+        let blakeposture = BlakeCryptoPosture::forbid_all();
+        CargoEnvDescriptor {
+            host_toolchain,
+            safety_neurorights,
+            dev_tunnel,
+            blake_posture: blakeposture,
+        }
+    }
+}
